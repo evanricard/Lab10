28bb3520a86dad9d08f8a8b14648287147916fce
Evan Ricard <evanricard11@gmail.com>
2017-11-06 07:08:56 -0500

    Lab 10 assignment complete with ascii art and class diagram.

---
+++ Lab10/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double price;
+        private int carlot;
+
+        public Car()
+        {
+
+        }
+
+        public Car(int carlot, string make, string model, int year, double price)
+        {
+            this.carlot = carlot;
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.price = price;
+        }
+
+
+        public string Make { get => make; set => make = value; }
+        public string Model { get => model; set => model = value; }
+        public int Year { get => year; set => year = value; }
+        public double Price { get => price; set => price = value; }
+
+
+        public override string ToString()
+        {
+            return $"{carlot}. {make,-5} {model,-5} {year,5} - {price,10:C}";
+        }
+
+    }
+}
---
+++ Lab10/UsedCar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class UsedCar : Car
+    {
+        double mileage;
+
+        public UsedCar()
+        {
+
+        }
+
+        public UsedCar(int carlot, string make, string model, int year, double price, double mileage) : base(carlot, make, model, year, price)
+        {
+            this.mileage = mileage;
+        }
+
+        public double Mileage { get => mileage; set => mileage = value; }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"\tMileage: {(mileage),10:n}" + " - Used Car";
+        }
+
+    }
+}
